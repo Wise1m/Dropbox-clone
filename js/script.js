da108a53af73333b987fb295a9951fe436919d9c
Wise1m <aribisalapraise12@gmail.com>
2022-11-10 12:54:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
-function display_1(){
-    document.getElementById('').src=''
-}
+var image = ["images/display1.png", "images/display2.JFIF", "images/display3.png", "images/display4.JFIF", "images/display5.png", 
+"images/display6.png", "images/display7.svg", "images/display8.png"]
 
 function display_1(){
-    document.getElementById('').src=''
+    document.getElementById('display').src='image[0]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function diplay_2(){
+    document.getElementById('display').src='image[1]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function display_3(){
+    document.getElementById('display').src='image[2]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function display_4(){
+    document.getElementById('display').src='image[3]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function display_5(){
+    document.getElementById('display').src='image[4]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function display_6(){
+    document.getElementById('display').src='image[5]'
 }
 
-function display_1(){
-    document.getElementById('').src=''
+function display_7(){
+    document.getElementById('display').src='image[6]'
+}
+
+function display_8(){
+    document.getElementById('display').src='image[7]'
 }
