e10cb086a4b73ae3c31d593bbe9092f1de57f5cd
Wise1m <aribisalapraise12@gmail.com>
2022-11-10 11:03:25 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
+
+function display_1(){
+    document.getElementById('').src=''
+}
